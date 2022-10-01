91b27db062328e8bb70e5e4494dfe415ac679f62
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-10-01 01:55:09 -0400

    adding reducer

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import modeReducer from './features/modelSlice'
 
 export const store = configureStore({
     reducer: {
-
+        mode: modeReducer
     }
 })
